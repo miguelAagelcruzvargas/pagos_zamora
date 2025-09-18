c6cedd91db6b964b9ac8288bafa93b6e725bd384
Miguel Angel Vargas <139577769+miguelAagelcruzvargas@users.noreply.github.com>
2025-09-17 21:27:55 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'gestion-raya-cache-v2';
+const PRECACHE_URLS = [
+  './',
+  'index.html',
+  'manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request))
+  );
+});
